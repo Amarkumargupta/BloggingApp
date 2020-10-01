5ed555aa4b5b0f909a0b2daa991e6c28e7440d49
AmarKumarGupta <amarmotioniit@gmail.com>
2020-10-01 19:43:21 +0530

    pagination is added

--- Blog/views.py
+++ Blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator ,EmptyPage,PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(status='published')
+    posts_list = Post.objects.all()
+    paginator = Paginator(posts_list,2)
+    page_no = request.GET.get('page')
+    try:
+        posts=paginator.page(page_no)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     context = {
         'title':'All Posts',
+        'page': page_no,
         'Posts':posts,
     }
     return render(request,'Blog/post/list.html',context)
 
 def post_detail(request,year,month,day,post):
-    post = get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    post = get_object_or_404(
+                    Post,
+                slug=post,
+                status='published',
+                publish__year=year,
+                publish__month=month,
+                publish__day=day     )
     context={
         'title':post.title,
         'post':post,
